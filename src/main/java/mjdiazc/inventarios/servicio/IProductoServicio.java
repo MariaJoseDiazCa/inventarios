06443898eca5eeb72b9a9d5c06efcd3be24a3a53
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2023-10-02 16:23:00 -0300

    UPDATE: add method agregarProducto()

--- src/main/java/mjdiazc/inventarios/servicio/IProductoServicio.java
+++ src/main/java/mjdiazc/inventarios/servicio/IProductoServicio.java
@@ -7,7 +7,7 @@ public interface IProductoServicio {
     public List<Producto> listarProductos();
 
     public Producto buscarProductoPorId(Integer idProducto);
-    public void guardarProducto(Producto producto);
+    public Producto guardarProducto(Producto producto);
 
     public void eliminarProductoPorId(Integer idProducto);
 
