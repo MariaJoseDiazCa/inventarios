133c304533a2827b5dccd548f09849f12b4347e8
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2023-10-03 12:09:38 -0300

    UPDATE: add method obtenerProductoPorId()

--- src/main/java/mjdiazc/inventarios/controlador/ProductoControlador.java
+++ src/main/java/mjdiazc/inventarios/controlador/ProductoControlador.java
@@ -1,10 +1,12 @@
 package mjdiazc.inventarios.controlador;
 
+import mjdiazc.inventarios.excepcion.RecursoNoEncontradoExcepcion;
 import mjdiazc.inventarios.modelo.Producto;
 import mjdiazc.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +33,16 @@ public class ProductoControlador {
     public Producto agregarProducto(@RequestBody Producto producto){
         logger.info("Producto a agregar: " + producto);
         return this.productoServicio.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(
+            @PathVariable int id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if(producto != null) {
+            return ResponseEntity.ok(producto);
+        } else {
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        }
     }
 }
